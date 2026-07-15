3ac5bfd251bd7266f1149568b1de0c23f408c39a
Raspberry Pi OS <kibetnat@gmail.com>
2026-07-15 10:14:58 +0300

    feat(sorter): create an adapter class for the sorter service

--- backend/sorting/sorter.py
+++ backend/sorting/sorter.py
@@ -1,39 +1,18 @@
-import pandas as pd
-import os
+import prefilter
+import sorter_function as sorter
+import pandas
 
 
-def sort_csv(path: str, sort_by: str):
-    try:
-        if not os.path.exists(path):
-            raise FileNotFoundError(f"The input file '{path}' does not exist")
-        file = pd.read_csv(path)
+class Adapter:
+    def __init__(self, path, sort_by, amount_col, min_value, max_value):
+        self.__upper: pandas.df = sorter.sort_csv(path, sort_by)
+        self.__lower: pandas.df = sorter.sort_csv(path, sort_by)
 
-        cols_to_check = [sort_by] if isinstance(sort_by, str) else sort_by
-        missing_cols = [col for col in cols_to_check if col not in file.columns]
-        if missing_cols:
-            raise KeyError(f"Column(s) {missing_cols} not found in the csv file")
-        sorted_data = file.sort_values(by="customer_id", ascending=True)
+        self.__upper = prefilter.filter_min(self.__upper, amount_col, min_value)
+        self.__lower = prefilter.filter_max(self.__lower, amount_col, max_value)
 
-        return sorted_data
-    except FileNotFoundError as e:
-        print(f"File Error: {e}")
-    except pd.errors.EmptyDataError:
-        print(f"Data Error: The file '{path}' is empty.")
-    except pd.errors.ParserError:
-        print(f"Parser Error: '{path}' is not a valid CSV format.")
-    except KeyError as e:
-        print(f"Sorting Error: {e}")
-    except PermissionError:
-        print(
-            "Permission Error: Cannot read or write to the specified path. Check your folder permissions."
-        )
-    except Exception as e:
-        print(f"An unexpected error occured: {e}")
+    def get_upper(self):
+        return self.__upper
 
-
-def main():
-    print(sort_csv("data/mlinzi_sample_transactions.csv", "customer_id"))
-
-
-if __name__ == "__main__":
-    main()
+    def get_lower(self):
+        return self.__lower
