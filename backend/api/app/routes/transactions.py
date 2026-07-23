0eb8e2e94d7210a352fc7d5fa6ba45da8649e50f
Raspberry Pi OS <kibetnat@gmail.com>
2026-07-23 11:43:16 +0300

    feat(api): create a service that populates the database on initial request

--- backend/api/app/routes/transactions.py
+++ backend/api/app/routes/transactions.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlmodel import Session, select
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
 from ..models.transactions import Transaction
 from typing import List, Optional
 from ..database import get_db
-from datetime import date, datetime, time
+from ..services.transactions import get_flagged_transactions
+from datetime import date
 
 router = APIRouter(prefix="/transactions/", tags=["Transactions"])
 
@@ -14,23 +15,4 @@ def flagged_transaction(
     target_date: date = Depends(date.today()),
     db: Session = Depends(get_db),
 ):
-    query = select(Transaction)
-    # filter for flag
-    if flag:
-        query = query.where(Transaction.flag == flag)
-    else:
-        query = query.where(Transaction.flag.is_not(None))
-
-    # filter for date
-    start_of_day = datetime.combine(target_date, time.min)
-    end_of_day = datetime.combine(target_date, time.max)
-    query = query.where(
-        Transaction.date_flagged >= start_of_day, Transaction.date_flagged <= end_of_day
-    )
-
-    results = db.exec(query).all()
-    if not results:
-        raise HTTPException(
-            status_code=404, detail="No transactions flagged for velocity"
-        )
-    return results
+    return get_flagged_transactions(db, target_date, flag)
